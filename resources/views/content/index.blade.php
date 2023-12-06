11176368e6b543151793e1ccac75f7b277f31359
Edwin-Jaya <edwinkudo.07@gmail.com>
2023-12-06 08:00:32 +0700

    First Commit

---
+++ resources/views/content/index.blade.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href={{asset("css/index.css")}}>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">DIIIGDAYA</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Apa itu DIIIGDAYA?</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Playlist</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="first-page">
+            <div class="text-center">
+                <img src="https://pbs.twimg.com/media/F8o8e7ZagAA7Qhm.jpg" class="img-fluid" alt="">
+            </div>    
+        </div>
+    </div>
+    <div class="container">
+        <div class="row d-flex justify-content-center p-4 p-md-4" id="section">
+            <div class="card border-light mb-3" style="max-width: 540em;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" class="img-fluid rounded-start" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Apa itu Digdaya?</h5>
+                            <p class="card-text">“Digdaya” adalah album rap bahasa Indonesia ketiga dari Laze, yang dirilis di bawah naungan Preachja Records. Album ini memiliki makna mendalam yang mencerminkan esensi dari kata “digdaya” dalam Kamus Besar Bahasa Indonesia, yang berarti tidak terkalahkan atau sakti.</p>
+                            <p class="card-text">Dalam album ini, Laze berkolaborasi dengan Liam Amadeo dalam lagu “Digdaya” yang menyoroti semangat dan tekad untuk mencapai kesuksesan. Lagu ini mengungkapkan dorongan untuk menghadapi tantangan dan mengatasi rintangan dalam perjalanan menuju tujuan.</p>        
+                            <p class="card-text">Laze berharap bahwa “Digdaya” dapat menjadi soundtrack perjuangan Anda semua melawan naik turunnya kehidupan sambil meneguhkan jiwa. Kata “Digdaya” sendiri dipilih Laze dengan pemaknaan kata “dig” dalam bahasa Inggris yang bermakna “gali.” Sedangkan “daya” dari bahasa Indonesia. Secara garis besar “Digdaya” dimaknai sebagai penggalian kekuatan.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="container">
+        <div class="second-page">
+            <h1 class="text-center">Playlist</h1>
+            <div class="row mt-4 mt-md-3">
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Tipu diri
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Porsi Curah (Feat. Ayub John)
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>  
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Tanpa Perayaan (Feat. Dzulfahmi & Rand Slam)
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+            </div>
+            <div class="row mt-4 mt-md-3">
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Belati Belati
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Kebal Ejekan
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>  
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Merah Muda Biru (Feat. Liam Amadeo)
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+            </div>
+            <div class="row mt-4 mt-md-3">
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Menciptakan Hening (Feat. Akara)
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Kabur (Interlude)
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>  
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Di Pinggir Jurang (Feat. Wicigo Shawty)
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+            </div>
+            <div class="row mt-4 mt-md-3">
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Yang Lebih
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Geppetto
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>  
+                </div>
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Tabah/Gubahanku
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+            </div>
+            <div class="row mt-4 mt-md-3">
+                <div class="col-md-4 col-sm-2 col-lg-4 mb-4 d-flex justify-content-center">
+                    <div class="card">
+                        <img class="card-img-top img-thumbnail" src="https://i.scdn.co/image/ab67616d0000b273777be63909854139c245c6c6" alt="" width="350vw" height="350vh">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Digdaya (Feat. Liam Amadeo)
+                            </h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae voluptates, perspiciatis autem, nemo obcaecati doloribus optio recusandae aliquam assumenda cupiditate aperiam exercitationem unde, incidunt voluptatem consectetur soluta laboriosam reprehenderit?
+                            </p>
+                        </div>
+                        <button type="button" class="btn btn-primary mt-3 mb-3 mx-auto" data-bs-toggle="modal" style="width: 10em;" data-bs-target="#staticBackdrop">
+                            Read More
+                        </button>
+                    </div>   
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
